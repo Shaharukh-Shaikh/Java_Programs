e1407ca632c8cece7d187bb8aa61fdec01d4ac82
Shaharukh-Shaikh <159990421+Shaharukh-Shaikh@users.noreply.github.com>
2024-05-27 22:46:34 +0530

    Add files via upload

---
+++ Alphabet.java
@@ -0,0 +1,19 @@
+class Alphabet 
+{
+	public static void main(String[] args) 
+	{
+		char a='A';
+		if(a>='a' && a<='z')
+		{
+			System.out.println(a+"   Is An Alphabet");		
+		}
+		else if(a>='A' && a<='Z')
+		{
+			System.out.println(a+"   Is An Alphabet");
+		}
+		else
+		{
+			System.out.println(a+"   Is Not And Alphabet");
+		}
+	}
+}
