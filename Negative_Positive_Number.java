c8048c6568b4cf4b9935e82f3042959627007068
Shaharukh-Shaikh <159990421+Shaharukh-Shaikh@users.noreply.github.com>
2024-05-27 23:13:04 +0530

    Add files via upload

---
+++ Negative_Positive_Number.java
@@ -0,0 +1,19 @@
+class Negative_Positive_Number
+{
+	public static void main(String[] args) 
+	{
+		int a=0;
+		if(a>0)
+		{
+			System.out.println("Number Is Positive");
+		}
+		else if(a<0)
+		{
+			System.out.println("Number Is Negative");
+		}
+		else
+		{
+			System.out.println("Number Is Zero");
+		}
+	}
+}
