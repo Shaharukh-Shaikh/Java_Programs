9a92990677c5fda48f5d021ac4b159848f3b423b
Shaharukh-Shaikh <159990421+Shaharukh-Shaikh@users.noreply.github.com>
2024-05-27 23:14:23 +0530

    Add files via upload

---
+++ Odd_Even_Number.java
@@ -0,0 +1,15 @@
+class Odd_Even_Number
+{
+	public static void main(String[] args) 
+	{
+		int num=10;
+		if(num%2==0)
+		{
+			System.out.println("Number Is Even : "+num);
+		}
+		else
+		{
+			System.out.println("Number Is Odd : "+num);
+		}
+	}
+}
