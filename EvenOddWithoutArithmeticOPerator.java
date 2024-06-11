8675cdac2a619ae2c7baa0c704de98bb6bbaebfe
Shaharukh-Shaikh <sharukhshaikh6596@gmail.com>
2024-06-11 21:52:55 +0530

    Even Odd Program Without Using The Arithmetic Operator

---
+++ EvenOddWithoutArithmeticOPerator.java
@@ -0,0 +1,10 @@
+public class EvenOddWithoutArithmeticOPerator {
+    public static void main(String[] args) {
+        int num=5;
+        if((num&1)==0){
+            System.out.println("Number Is Even Number");
+        }else{
+            System.out.println("Number Is Odd Number");
+        }
+    }
+}
