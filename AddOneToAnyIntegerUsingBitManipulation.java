9a2b432135d64bab5d08184b83f5898f12796111
Shaharukh-Shaikh <sharukhshaikh6596@gmail.com>
2024-06-17 17:47:04 +0530

    adding file

---
+++ AddOneToAnyIntegerUsingBitManipulation.java
@@ -0,0 +1,10 @@
+/**
+ * AddOneToAnyIntegerUsingBitManipulation
+ */
+public class AddOneToAnyIntegerUsingBitManipulation {
+
+    public static void main(String[] args) {
+        int x = 6;
+        System.out.println(x + " + " + 1 + " is " + -~x);
+    }
+}
